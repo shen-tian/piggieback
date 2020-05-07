3750bf4828ba1e08f8892dff36cd65a0860973bc
Shen Tian <biozinc@gmail.com>
2020-05-07 13:04:55 +0200

    Make piggieback dependent on print

--- src/cider/piggieback.clj
+++ src/cider/piggieback.clj
@@ -3,7 +3,8 @@
   {:author "Chas Emerick"}
   (:refer-clojure :exclude [load-file])
   (:require
-   [nrepl.middleware :refer [set-descriptor!]]))
+   [nrepl.middleware :refer [set-descriptor!]]
+   [nrepl.middleware.print :as print]))
 
 (defmacro ^:private if-ns
   [ns body else]
@@ -20,7 +21,7 @@
        (load "piggieback_shim"))
 
 (set-descriptor! #'wrap-cljs-repl
-                 {:requires #{"clone"}
+                 {:requires #{"clone" #'print/wrap-print}
                   ;; piggieback unconditionally hijacks eval and load-file
                   :expects #{"eval" "load-file"}
                   :handles {}})
